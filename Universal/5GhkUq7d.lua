794f8d3fb8845e7c6b42795f4ef7bfe3a4f4d651
X_D0xR <109306431+K1rina@users.noreply.github.com>
2023-12-23 22:35:30 +0700

    Create 5GhkUq7d.lua

---
+++ Universal/5GhkUq7d.lua
@@ -0,0 +1,7 @@
+local ImageLabel = Instance.new("ImageLabel")
+ImageLabel.Parent = game:GetService("CoreGui")
+ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageLabel.BorderSizePixel = 0
+ImageLabel.Size = UDim2.new(0, 784, 0, 490)
+ImageLabel.Image = "rbxassetid://9419562118"

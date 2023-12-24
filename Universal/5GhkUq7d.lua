a343ba10e91236896b79c448d3661ffcbc139d45
Aqsyn <AqsynSoCute@gmail.com>
2023-12-24 17:58:09 +0700

    forgot

--- Universal/5GhkUq7d.lua
+++ Universal/5GhkUq7d.lua
@@ -1,7 +1,9 @@
-local ImageLabel = Instance.new("ImageLabel")
-ImageLabel.Parent = game:GetService("CoreGui")
-ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-ImageLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
-ImageLabel.BorderSizePixel = 0
-ImageLabel.Size = UDim2.new(0, 784, 0, 490)
-ImageLabel.Image = "rbxassetid://9419562118"
+if not game:GetService("CoreGui"):FindFirstChild("ImageLabel") then
+    local ImageLabel = Instance.new("ImageLabel")
+    ImageLabel.Parent = game:GetService("CoreGui")
+    ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+    ImageLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    ImageLabel.BorderSizePixel = 0
+    ImageLabel.Size = UDim2.new(0, 784, 0, 490)
+    ImageLabel.Image = "rbxassetid://9419562118"
+end

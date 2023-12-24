1d7d2e7fca4fb1e56223b395403a03107439df15
Aqsyn <AqsynSoCute@gmail.com>
2023-12-24 22:38:05 +0700

    fix

--- Universal/5GhkUq7d.lua
+++ Universal/5GhkUq7d.lua
@@ -1,9 +1,10 @@
-if not game:GetService("CoreGui"):FindFirstChild("ImageLabel") then
-    local ImageLabel = Instance.new("ImageLabel")
-    ImageLabel.Parent = game:GetService("CoreGui")
-    ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    ImageLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
-    ImageLabel.BorderSizePixel = 0
-    ImageLabel.Size = UDim2.new(0, 784, 0, 490)
-    ImageLabel.Image = "rbxassetid://9419562118"
-end
+local ScreenGui = Instance.new("ScreenGui")
+local ImageLabel = Instance.new("ImageLabel")
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+ImageLabel.Parent = ScreenGui
+ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageLabel.BorderSizePixel = 0
+ImageLabel.Size = UDim2.new(0, 784, 0, 490)
+ImageLabel.Image = "rbxassetid://9419562118"
